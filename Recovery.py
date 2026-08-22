c9c826e24e5b4b6cb1d30ec4252c14edc84dc9b2
Muhammad Hassaan Zahid <85370770+Iamhasaanzahid@users.noreply.github.com>
2026-08-22 14:25:14 +0500

    Update Recovery.py

--- Recovery.py
+++ Recovery.py
@@ -1,5 +1,18 @@
+import streamlit as st
+
+
 class RecoveryAndRemediation:
 
-    def recover_systems(self):
-        print("Systems recover kiye ja rahay hain.")
+    def recover_system(self):
+        st.write("### Incident Response: Recovery Phase")
+        st.info("Initiating system recovery protocols...")
+
+        # Simulating recovery steps
+        st.write("[+] Isolating compromised host from network.")
+        st.write(
+            "[+] Restoring system configurations from secure backup."
+        )
+        st.write("[+] Updating firewall rules to block malicious IPs.")
+
+        st.success("Full recovery achieved and system is secure.")
         return "Full recovery achieved."
